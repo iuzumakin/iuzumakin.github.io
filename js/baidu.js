66e6a4d6876fa38313f38dd61a3e10b18648e038
xVan <iuzumakin@gmail.com>
2019-04-28 00:27:29 +0800

    remember

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0b14a40a56a536af308e033c6f5a3775";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
